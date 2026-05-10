c7c1b12029ec0d9773184b074c698d10c9746e37
Your Name <your-email@example.com>
2026-05-10 11:43:22 -0400

    working on env

---
+++ ocean/auv_sdf/auv_sdf.h
@@ -0,0 +1,159 @@
+
+#include <stdlib.h>
+#include <math.h>
+#include "raylib.h"
+#include <sdf_constants.h>
+
+typedef struct {
+    float perf;
+    float score;
+    float episode_return;
+    float episode_length;
+    float n;
+} Log;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vec3;
+
+typedef struct {
+    float w;
+    float x;
+    float y;
+    float z;
+} Quat;
+
+typedef struct {
+    Vec3 pos;
+    Quat quat;
+} DroneState;
+
+typedef struct {
+    Log log;
+    DroneState DroneState;
+    float* observations;
+    float* actions;
+    float* rewards;
+    float* terminals;
+
+    int num_agents;
+    int tick;
+    int max_ticks;
+    float episode_return;
+
+    unsigned int rng;
+    // Add AUV state here.
+} AUVSDF;
+
+inline float sdf_ocean_surface(AUVSDF* env){
+float x = env->DroneState.pos.x;
+float y = env->DroneState.pos.y;
+float z = env->DroneState.pos.z;
+return y;
+}
+
+inline float sdf_ocean_floor(AUVSDF* env){
+float x = env->DroneState.pos.x;
+float y = env->DroneState.pos.y;
+float z = env->DroneState.pos.z;
+return ocean_floor_base - y;
+}
+
+inline float map_ocean(AUVSDF* env){
+    float x = env->DroneState.pos.x;
+    float y = env->DroneState.pos.y;
+    float z = env->DroneState.pos.z;
+
+    // 1. Call your inline primitives
+    float dist_surface = sdf_ocean_surface(env);
+    float dist_floor = sdf_ocean_floor(env);
+    
+    // 2. Combine them. 
+    // fmaxf guarantees the agent is bounded by whichever surface is closest.
+    return fmaxf(dist_surface, dist_floor);
+}
+
+inline Vec3 get_drone_unit_vector(AUVSDF* env){
+
+    //Typical robotics convention is define foward along the
+    //positive X-axis in local or body frame xyz, 1, 0, 0
+    float qw = env->DroneState.quat.w;
+    float qx = env->DroneState.quat.x;
+    float qy = env->DroneState.quat.y;
+    float qz = env->DroneState.quat.z;
+
+    Vec3 forward;
+    forward.x = 1.0f - 2.0f * (qy * qy + qz * qz);
+    forward.y = 2.0f * (qx * qy + qw * qz);
+    forward.z = 2.0f * (qx * qz - qw * qy);
+
+    return forward;
+}
+
+inline float sphere_marching_distance(AUVSDF* env, Vec3* unit_vec){
+
+    float dist_jumped = map_ocean(env);
+
+    float next_jump_x = dist_jumped * unit_vec->x;
+    float next_jump_y = dist_jumped * unit_vec->y;
+    float next_jump_z = dist_jumped * unit_vec->z;
+
+    float x_new = env->DroneState.pos.x + next_jump_x;
+    float y_new = env->DroneState.pos.y + next_jump_y;
+    float z_new = env->DroneState.pos.z + next_jump_z;
+
+    for (int i = 0; i < max_jumps; i++){
+        
+        float next_jump_x = dist_jumped * unit_vec->x;
+        float next_jump_y = dist_jumped * unit_vec->y;
+        float next_jump_z = dist_jumped * unit_vec->z;
+
+
+    }
+
+
+}
+
+//Now we need to write the sphere marching math here
+
+void render(AUVSDF* env){
+    float x = env->DroneState.pos.x;
+    float y = env->DroneState.pos.y;
+    float z = env->DroneState.pos.z;
+
+    for (int pixel_y = 0; pixel_y < height; pixel_y++){
+        for (int pixel_x = 0; pixel_x < width; pixel_x++){
+            float u = (pixel_x - width / 2.0f) / (height / 2.0f);
+            float v = (height / 2.0f - pixel_y) / (height / 2.0f);
+
+            Vector3 raw_dir = {u, v, focal_length};
+
+            //Vector3 is a raylib struct that looks like this. This is why we can use raw_dir.x for math below
+            // typedef struct Vector3 {
+            //     float x;
+            //     float y;
+            //     float z;
+            // } Vector3;
+            //
+            // So this:
+            // Vector3 raw_dir = {u, v, focal_length};
+            //
+            // means:
+            // raw_dir.x = u;
+            // raw_dir.y = v;
+            // raw_dir.z = focal_length;
+            //
+            // That is why this works:
+
+            //First we calcualte then length and then we divide by the length to normalize
+            float length = sqrtf(raw_dir.x * raw_dir.x + raw_dir.y * raw_dir.y + raw_dir.z * raw_dir.z);
+            Vector3 ray_dir = {
+            raw_dir.x / length,
+            raw_dir.y / length,
+            raw_dir.z / length};
+
+        }
+    }
+}
---
+++ ocean/auv_sdf/sdf_constants.h
@@ -0,0 +1,14 @@
+
+//This is where we will define constants for the sdf equation
+//We will also define units here, we are going to be working in meters
+//This will be important for our physics calcs
+
+const float ocean_floor_base = -100.0;
+
+//Raylib Constants for how we visualize
+const int width = 1920;
+const int height = 1080;
+const float focal_length = 1.0f; // Roughly 90-degree FOV
+
+//Sphere marching constants
+const int max_jumps = 10;
